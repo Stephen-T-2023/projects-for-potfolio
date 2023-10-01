ef124bb744bc267b259a358b39abaac11a7eed9b
Stephen-T-2023 <144911216+Stephen-T-2023@users.noreply.github.com>
2023-10-01 19:43:14 +0100

    Add files via upload

--- Hangman.py
+++ Hangman.py
@@ -5,18 +5,18 @@ Graphics = (
 """,
 
 """
-    _
-    |
-    |
-    |
-    |
-    |
-    |
-    |
+    
+    
+    
+    
+    
+    
+    
+    ___________
     """,
 
 """
-    _
+    
     |
     |
     |
@@ -114,46 +114,54 @@ Graphics = (
     |__________
     """)
 
-wrong = 0
+quits = ""
 
-print("Welcome to hangman!\n")
+while quits != "q":
+    print("Welcome to hangman!\n")
 
-with open("hangman_list.txt", "r") as file:
-    allText = file.read()
-    words = list(map(str, allText.split()))
-word = random.choice(words)
+    wrong = 0
 
-guessed_word = list("_" * len(word))
-lives = 10
-wordGuessed = False
+    with open("hangman_list.txt", "r") as file:
+        allText = file.read()
+        words = list(map(str, allText.split()))
+    word = random.choice(words)
 
-while lives>=1 and not wordGuessed == True:
+    guessed_word = list("_" * len(word))
+    lives = 10
+    wordGuessed = False
 
-    print(" ".join(guessed_word))
+    incorrectletters = []
 
-    user_guess = input("Guess a letter / word! ("+str(lives)+" lives remaining) (Guess the whole word after all letters have been filled) ")
+    while lives>=1 and not wordGuessed == True:
 
-    user_guess = user_guess.lower()
-
-    letter_in_word = False
-    for i in range(len(word)):
-        if user_guess == word[i]:
-            guessed_word[i] = user_guess
-            letter_in_word = True
-    
-    if letter_in_word == False and user_guess != word:
-        lives -= 1
-        print("You guessed incorrectly")
-        wrong = wrong + 1
-        print (Graphics[wrong])
-
-    if user_guess == word:
         print(" ".join(guessed_word))
-        print("You have guessed the word correctly")
-        wordGuessed = True
-        quit
 
-    elif lives < 1:
-        print("You have failed to guess the word")
-        print("The word was ",word)
-        quit
+        user_guess = input("Guess a letter / word! ("+str(lives)+" lives remaining) (Guess the whole word after all letters have been filled) ").lower()
+
+        letter_in_word = False
+        for i in range(len(word)):
+            if user_guess == word[i]:
+                guessed_word[i] = user_guess
+                letter_in_word = True
+        
+        if letter_in_word == False and user_guess != word:
+            lives -= 1
+            print("You guessed incorrectly")
+            wrong = wrong + 1
+            print (Graphics[wrong])
+            incorrectletters.append(user_guess)
+            print("Previously guessed letters are: ", ( incorrectletters))
+
+        if user_guess == word:
+            print(" ".join(guessed_word))
+            print("You have guessed the word correctly")
+            wordGuessed = True
+            quits = input("Press q to quit or enter to continue")
+            quits = quits.lower()
+
+        elif lives < 1:
+            print("You have failed to guess the word")
+            print("The word was ",word)
+            quits = input("Press q to quit or enter to continue")
+            quits = quits.lower()
+quit
